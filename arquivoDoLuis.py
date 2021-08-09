fb99b422da52449d45e4c8d5d17b7a84402575ca
Luís Fernando Teixeira Bicalho <lfernandotexbicalho@hotmail.com>
2021-08-09 20:44:31 -0300

    Adicionando definição de sexo masculino e feminino para a funcaoBoladona

--- arquivoDoLuis.py
+++ arquivoDoLuis.py
@@ -1,4 +1,9 @@
-def funcaoBoladona(nome):
-    print(f"{nome} Boladão")
+def funcaoBoladona(nome, sexo):
+    if sexo == "M":
+        print(f"{nome} Boladão")
+    else:
+        print(f"{nome} Boladona")
 
-funcaoBoladona("Brian")
+funcaoBoladona("Brian", "M")
+funcaoBoladona("Luís", "M")
+funcaoBoladona("Larissa", "F")
